7b90344e6b26a66c1f48cce550b3702fe03e8397
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-02-10 04:03:25 +0530

    s

---
+++ PaytmKit/lib/config.php
@@ -0,0 +1,4 @@
+<?php
+define('MERCHENT_KEY','j#GT5u%@AMHk3c%T');
+define('MERCHENT_MID','aPymau65978811081015');
+define('ENV','TEST')
---
+++ index.php
@@ -0,0 +1,475 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kundan Dark world</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- box icons -->
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
+
+
+</head>
+<body>
+    <!-- navbar -->
+    <header>
+        <a href="#" class="logo">
+            <i class='bx bx-movie-play' ></i>Kundan Dark world
+        </a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <!-- menu by kundan -->
+        <ul class="navbar">
+            <li><a href="#home"class="home-active">Home</a></li>
+            <li><a href="#movies">Movies</a></li>
+            <li><a href="#coming">Coming</a></li>
+            <li><a href="#newsletter">Newsletter</a></li>
+        </ul>
+        <a href="#" class="btn">Sign In</a>
+        <a href="https://searchyourmovienow.netlify.app/" class ="btn">Search Movies</a>
+    </header>
+    <!-- Home -->
+    <section class="home swiper" id="home">
+        <div class="swiper-wrapper">
+        <div class="swiper-slide container"><img src="img/MV5BNTk2MjUxMjEtZDU5OC00MzYxLTkwMWYtMGM5YWQ0MWMyN2I1XkEyXkFqcGdeQXVyMTE0MzY0NjE1._V1_.jpg" alt=""><div class="home-text"><span>TRENDDING</span>
+        <!-- book now can be used to buy this -->
+        <h1>Rocketry<br></h1><a href="pay.html" class="btn">Book Now</a>
+        <a href="#" class="play">
+            <i class='bx bx-play'></i></a></div></div>
+            <!-- box 2 -->
+  <div class="swiper-slide container"><img src="img/qw (2).jpg" alt=""><div class="home-text"><span>TRENDDING</span>
+        <!-- book now can be used to buy this -->
+        <h1>Anbe Sivam<br></h1><a href="#" class="btn">Book Now</a>
+        <a href="#" class="play">
+            <i class='bx bx-play'></i></a></div></div> 
+    <!-- box 3       -->
+     <div class="swiper-slide container"><img src="img/home3.jpg" alt=""><div class="home-text"><span>TRENDING</span>
+        <!-- book now can be used to buy this -->
+        <h1>Spiderman <br>Be carnage</h1><a href="#" class="btn">Book Now</a>
+        <a href="#" class="play">
+            <i class='bx bx-play'></i></a></div></div>
+        <!-- home box 4 -->
+        <!-- below code is used to make movie boxes -->
+ <div class="swiper-slide container"><img src="img/IMG_20220920_223353_031.jpg"><div class="home-text"><span>TRENDING</span>
+        <!-- book now can be used to buy this -->
+        <h1>Avenger <br>End Game</h1><a href="#" class="btn">Book Now</a>
+        <a href="pay.index" class="play">
+            <i class='bx bx-play'></i></a></div></div> 
+<!-- above line code is use to make boxes of home movie -->
+            </div>
+    <div class="swiper-pagination"></div>
+        
+    </section>
+ <!-- Swiper -->
+    
+      <!-- <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1<div>
+        <div class="swiper-slide">Slide 2<div>
+        <div class="swiper-slide">Slide 3<div>
+        <div class="swiper-slide">Slide 4<div>
+        <div class="swiper-slide">Slide 5<div>
+        <div class="swiper-slide">Slide 6<div>
+        <div class="swiper-slide">Slide 7<div>
+        <div class="swiper-slide">Slide 8<div>
+        <div class="swiper-slide">Slide 9<div>
+      </div> -->
+      <!-- <div class="swiper-button-next"></div> -->
+      <!-- <div class="swiper-button-prev"></div> -->
+      <!-- <div class="swiper-pagination"></div>
+    </div> -->
+<!-- movies section by kundan -->
+<section class="movies" id="movies">
+    <h2 class="heading">opening this week</h2>
+    <!-- movies container -->
+    <div class="movies-container">
+        <!-- box 1 of upcoming this week -->
+        <!-- below code used to make boxes -->
+        <div class="box">
+        <div class="box-img">
+        <a href="http://kundanbest.epizy.com/">
+        <img src="img/m1.jpg" alt=""></a>
+    </div>
+    <a href="http://kundanbest.epizy.com/">Watch</a>
+    <span>120 min |Action</span>
+        </div>
+    <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m2.jpg" alt="">
+    </div>
+    <h3>Dunkirk</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+<!-- box3 -->
+ <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 3 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m3.jpg" alt="">
+    </div>
+    <h3>Batman Vs Super-Man</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+     <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 4-->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m4.jpg" alt="">
+    </div>
+    <h3>john Wick 2</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 5 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m5.jpg" alt="">
+    </div>
+    <h3>Aquaman</h3>
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m6.jpg" alt="">
+    </div>
+    <h3>Black Panther</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m7.jpg" alt="">
+    </div>
+    <h3>THOR</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m8.png" alt="">
+    </div>
+    <h3>Bumblebee</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+     <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m9.jpg" alt="">
+    </div>
+    <h3>Mortal Engines</h3>
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+ <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m10.jpg" alt="">
+    </div>
+    <h3>UnderWorld Blood WAR%S</h3> 
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="box">
+        <div class="box-img">
+        <img src="img/m2.jpg" alt="">
+    </div>
+    <h3>Dunkirk</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+    </div>
+</section>
+<!-- coming movies section -->
+<section class="coming" id="coming">
+    <h2 class="heading">coming soon</h2>
+</section>
+<!-- COMING CONTAINER OF MOVIES -->
+<div class="coming-container swiper">
+    <div class="swiper-wrapper">
+<!-- box 1 -->
+ <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m1.jpg" alt="">
+    </div>
+    <h3>Venom</h3>
+    <span>120 min |Action</span>
+        </div>
+    <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m2.jpg" alt="">
+    </div>
+    <h3>Dunkirk</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+<!-- box3 -->
+ <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 3 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m3.jpg" alt="">
+    </div>
+    <h3>Batman Vs Super-Man</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+     <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 4-->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m4.jpg" alt="">
+    </div>
+    <h3>john Wick 2</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 5 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m5.jpg" alt="">
+    </div>
+    <h3>Aquaman</h3>
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m6.jpg" alt="">
+    </div>
+    <h3>Black Panther</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m7.jpg" alt="">
+    </div>
+    <h3>THOR</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m8.png" alt="">
+    </div>
+    <h3>Bumblebee</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+     <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m9.jpg" alt="">
+    </div>
+    <h3>Mortal Engines</h3>
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+ <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/m10.jpg" alt="">
+    </div>
+    <h3>UnderWorld Blood WAR%S</h3> 
+    <span>120 min |Action</span>
+       </div>
+       </div>
+        </div>
+<section class="coming" id="coming">
+    <h2 class="heading">coming soon</h2>
+</section>
+<!-- COMING CONTAINER OF MOVIES -->
+<div class="coming-container swiper">
+    <div class="swiper-wrapper">
+<!-- box 1 -->
+ <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming1.jpg" alt="">
+    </div>
+    <h3>Venom</h3>
+    <span>120 min |Action</span>
+        </div>
+    <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming2.jpg" alt="">
+    </div>
+    <h3>Moon Knight</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+<!-- box3 -->
+ <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 3 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming3.jpg" alt="">
+    </div>
+    <h3>Rampage</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+     <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 4-->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming4.jpg" alt="">
+    </div>
+    <h3>The Iron Lady</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 5 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming5.jpg" alt="">
+    </div>
+    <h3>Justice League</h3>
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming6.jpg" alt="">
+    </div>
+    <h3>Doctor Strange</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming7.jpg" alt="">
+    </div>
+    <h3>Marvel Captain</h3>
+    <span>120 min |Adventure</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+       <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming8.jpg" alt="">
+    </div>
+    <h3>Black Widow</h3>
+    <span>120 min |Thriller</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+     <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming9.jpg" alt="">
+    </div>
+    <h3>Ant MAn</h3>
+    <span>120 min |Action</span>
+        </div>
+      <!-- above code is used to make boxes of movie -->
+ <!-- above code is used to make boxes of movie -->
+    <!-- below code used to make boxes -->
+    <!-- box 2 -->
+       <div class="swiper-slide box">
+        <div class="box-img">
+        <img src="img/coming10.jpg" alt="">
+    </div>
+    <h3>Avenger The Fallen</h3> 
+    <span>120 min |Action</span>
+       </div>
+       </div>
+        </div>
+</section>
+ <!-- section for newslatter -->
+ <section class="newsletter" id="newsletter">
+ <h2>Subscribe to Get <br> Newsletter</h2>
+ <form action="">
+     <input type="email" class="email"placeholder="Enter Email...." required>
+  <input type="submit" value="Subscribe"class="btn">
+</form>
+ </section>
+ <!-- now its time for footer -->
+ <!-- <Footer></Footer> -->
+ <section class="footer">
+     <a href="#" class="logo">
+<i class='bx bx-movie-play' ></i>Kundan Dark world
+</a>
+<div class="social">
+    <a href="#"><i class='bx bxl-facebook'></i></a>
+     <a href="#"><i class='bx bxl-twitter'></i></a>
+      <a href="#"><i class='bx bxl-instagram'></i></a>
+       <a href="#"><i class='bx bxl-tiktok'></i></a>
+        <a href="#"><i class='bx bxl-github'></i></a>
+</div>
+ </section>
+ <!-- copyright -->
+ <div class="copyright">
+     <p>&#169; Kundan Agrawal Cooperation Limited All Right Reserved.</p>
+ </div>
+     <!-- Swiper JS -->
+     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+     <script src="main.js"></script>
+
+</body>
+</html>
